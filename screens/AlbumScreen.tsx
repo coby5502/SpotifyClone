0dd0aa8a64b817c4169d20553c39a15d9620e71e
Doyoung <coby5502@gmail.com>
2021-04-30 02:55:31 +0900

    SongList

---
+++ screens/AlbumScreen.tsx
@@ -0,0 +1,27 @@
+import React, {useEffect} from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+
+import SongListItem from '../components/SongListItem';
+import albumDetails from '../data/albumDetails';
+
+const AlbumScreen = () => {
+
+    const route = useRoute();
+
+    useEffect(() => {
+        console.log(route);
+    }, [])
+
+    return (
+        <View>
+            <FlatList 
+                data={albumDetails.songs}
+                renderItem={({ item }) => <SongListItem song={item} />}
+                keyExtractor={( item ) => item.id}
+            />
+        </View>
+    )
+}
+
+export default AlbumScreen;
